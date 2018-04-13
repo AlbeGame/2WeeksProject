6ce7dfb3a522e1b056b4bdbe57275900e0b5d81d
AlbertoDesktop <alberto.paparella.88@gmail.com>
2018-04-13 23:00:42 +0200

    Fatta transizione di scene base

---
+++ Assets/Scripts/LevelTransition.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class LevelTransition : MonoBehaviour {
+
+    public int SceneIndex = -1;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (SceneIndex == -1)
+            return;
+
+        if (collision.collider.GetComponent<MainCharacter>() != null)
+            SceneManager.LoadScene(SceneIndex);
+    }
+}
